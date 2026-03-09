5d35843410e48e745d47f3aeb01eb1fbaa3bebe0
xcho7i <satoharuto961@gmail.com>
2026-03-09 19:54:14 -0400

    chore: release 4.3.0

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,6 +1,6 @@
 object Constants {
-	const val VERSION_NAME = "4.2.2"
-    const val VERSION_CODE = 40202
+	const val VERSION_NAME = "4.3.0"
+    const val VERSION_CODE = 40300
     const val TARGET_SDK = 36
     const val MIN_SDK = 26
     const val APP_ID = "com.zaneschepke.wireguardautotunnel"
